e94251fdbc20fadf60860070c1e8b9d625058dbf
Aiden <rheech22@gmail.com>
2025-11-02 18:30:11 +0900

    chapter1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Web Browser Engineering
+
+https://browser.engineering/
---
+++ py/main.py
@@ -0,0 +1,42 @@
+import socket
+
+
+class URL:
+    scheme: str
+    host: str
+    path: str
+
+    def __init__(self, url: str):
+        self.scheme, url = url.split('://', 1)
+        assert self.scheme == 'http'
+        if '/' not in url:
+            url += '/'
+        self.host, url = url.split('/', 1)
+        self.path = '/' + url
+
+    def request(self):
+        s = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+            proto=socket.IPPROTO_TCP,
+        )
+        s.connect((self.host, 80))
+        request = 'GET {} HTTP/1.0\r\n'.format(self.path)
+        request += 'Host: {}\r\n'.format(self.host)
+        request += '\r\n'
+        _ = s.send(request.encode('utf-8'))
+        response = s.makefile('r', encoding='utf-8', newline='\r\n')
+        statusline = response.readline()
+        version, status, explanation = statusline.split(' ', 2)
+        response_headers = {}
+        while True:
+            line = response.readline()
+            if line == '\r\n':
+                break
+            header, value = line.split(': ', 1)
+            response_headers[header.casefold()] = value.strip()
+        assert 'transfer-encoding' not in response_headers
+        assert 'content-encoding' not in response_headers
+        body = response.read()
+        s.close()
+        return body
